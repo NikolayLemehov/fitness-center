f78a62dbcf2a96a480f2b234cdcbb43e98690957
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-03-19 12:14:21 +0200

    Добавляет season-tickets desktop смену сетов при смене табов

--- source/js/season-tickets.js
+++ source/js/season-tickets.js
@@ -1,7 +1,11 @@
 export default class SeasonTickets {
   constructor() {
     this.tabs = document.querySelectorAll(`.season-tickets__tabs-item > button`);
-    this.isAllExisting = this.tabs.length > 0;
+    this.sets = document.querySelectorAll(`.season-tickets__offers-item`);
+    this.isAllExisting = this.tabs.length > 0 && this.sets.length > 0 && this.tabs.length === this.sets.length;
+    this.currentTab = null;
+    this.numberToTabSet = new Map();
+    this.tabToSet = new Map();
   }
 
   active() {
@@ -9,7 +13,20 @@ export default class SeasonTickets {
       return;
     }
 
-    this.tabs.forEach((tab) => {
+    this.tabs.forEach((tab, i) => {
+      if (tab.parentElement.classList.contains(`season-tickets__tabs-item--active`)) {
+        this.currentTab = i;
+      }
+      this.numberToTabSet.set(i, {
+        number: i,
+        tab,
+        set: this.sets[i],
+      });
+      this.tabToSet.set(tab, {
+        number: i,
+        set: this.sets[i],
+      });
+
       tab.addEventListener(`click`, (evt) => {
         evt.preventDefault();
         if (tab.parentElement.classList.contains(`season-tickets__tabs-item--active`)) {
@@ -17,7 +34,23 @@ export default class SeasonTickets {
         }
         this._onViewChange();
         tab.parentElement.classList.add(`season-tickets__tabs-item--active`);
+
+        this.numberToTabSet.get(this.currentTab).set.classList.remove(`season-tickets__offers-item--active`);
+        this.tabToSet.get(tab).set.classList.add(`season-tickets__offers-item--active`);
+        this.currentTab = this.tabToSet.get(tab).number;
       });
+    });
+
+    this.sets.forEach((set, i) => {
+      if (i === this.currentTab) {
+        if (!set.classList.contains(`season-tickets__tabs-item--active`)) {
+          set.classList.add(`season-tickets__tabs-item--active`);
+        }
+      } else {
+        if (set.classList.contains(`season-tickets__tabs-item--active`)) {
+          set.classList.remove(`season-tickets__tabs-item--active`);
+        }
+      }
     });
   }
 
